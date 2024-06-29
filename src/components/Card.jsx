b1acdb1bb7e1e55ab9d913fa996bc09af75bc6cb
[Karina] <[vip.kara0998@mail.ru]>
2024-06-29 18:51:31 +0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Card({ card, index, onClick }) {
+    return (
+        <div className={`card ${card.isFlipped ? "flipped" : ""}`}
+            onClick={() => onClick(index)}
+            >
+                <div className="card-inner">
+                    <div className="card-front">
+                       <img src={card.image} alt="card front of Karina" />
+                    </div>
+                    <div className="card-back">
+                    </div>
+                </div>
+        </div>
+    )
+}
